b054c683f7882d32352167a977a6a1904a5483fb
LeCongNam <namlem4u@gmail.com>
2022-03-08 14:37:32 +0700

    fix connect db

--- src/model/config/connect.js
+++ src/model/config/connect.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-
+var url ="mongodb+srv://lecongnam:Vobichuyen0612@cluster0.j3gct.mongodb.net/clean-blog?retryWrites=true&w=majority"
 
 async function Connect() {
     try {
-        await mongoose.connect(process.env.URI);
+        await mongoose.connect(process.env.URI |url);
         console.log('Connect Successfully');
     } catch (error) {
         console.log('Connected Failse: ',error);
